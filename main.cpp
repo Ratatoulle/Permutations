955f9c47d1a001d63312aa59b8cbdf2eb18e9973
Ratatoulle <1vasilev.andrey@gmail.com>
2022-06-01 23:07:12 +0500

    Init commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int* a, int i, int j)
+{
+    int s = a[i];
+    a[i] = a[j];
+    a[j] = s;
+}
+
+bool NextSet(int* a, int n)
+{
+    int j = n - 2;
+    while (j != -1 && a[j] >= a[j + 1]) 
+        j--;
+    if (j == -1)
+        return false; // больше перестановок нет
+    int k = n - 1;
+    while (a[j] >= a[k])
+        k--;
+    swap(a, j, k);
+    int l = j + 1, r = n - 1; // сортируем оставшуюся часть последовательности
+    while (l < r)
+        swap(a, l++, r--);
+    return true;
+}
+
+void Print(int* a, int n)  // вывод перестановки
+{
+    static int num = 1; // номер перестановки
+    cout.width(3); // ширина поля вывода номера перестановки
+    cout << num++ << ": ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int n, * A;
+    cout << "Enter the number of permutations: ";
+    cin >> n;
+    A = new int[n];
+    for (int i = 0; i < n; i++)
+        A[i] = i + 1;
+    Print(A, n);
+    while (NextSet(A, n))
+        Print(A, n);
+    return 0;
+}
